5e4a3521b61b18afe07cd2391e083311d604071b
Nguyen Tran Lan Trinh <lantrinh252002@gmail.com>
2023-04-08 23:23:39 +0700

    Gán role cho user

--- ATBM_HTTT-N11/CODE/QL_DEAN/PHANHE1/GanRoleChoUser.cs
+++ ATBM_HTTT-N11/CODE/QL_DEAN/PHANHE1/GanRoleChoUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,7 @@ namespace PHANHE1
 {
     public partial class GanRoleChoUser : Form
     {
+        string connectionString = Login.connectionString;
         public GanRoleChoUser()
         {
             InitializeComponent();
@@ -19,12 +21,65 @@ namespace PHANHE1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            OracleConnection conn = new OracleConnection(connectionString);
+            conn.Open();
+            try
+            {
 
+                DataTable dt2 = new DataTable();
+                string temp = "SELECT * FROM dba_users WHERE USERNAME NOT LIKE '%SYS%' AND ACCOUNT_STATUS = 'OPEN' ORDER BY created DESC";
+                OracleCommand Cmd = new OracleCommand(temp, conn);
+                Cmd.CommandType = CommandType.Text;
+                OracleDataAdapter da2 = new OracleDataAdapter(Cmd);
+                da2.Fill(dt2);
+                comboBoxUserName.DisplayMember = dt2.Columns[0].ColumnName;
+                comboBoxUserName.ValueMember = dt2.Columns[0].ColumnName;
+                comboBoxUserName.DataSource = dt2;
+            }
+
+            catch
+            {
+                MessageBox.Show("Không tồn tại user nào để gán Role!");
+                this.Close();
+            }
+            try
+            {
+
+                DataTable dt2 = new DataTable();
+                string temp = "SELECT DISTINCT GRANTED_ROLE FROM DBA_ROLE_PRIVS WHERE GRANTED_ROLE LIKE 'RL_%'";
+                OracleCommand Cmd = new OracleCommand(temp, conn);
+                Cmd.CommandType = CommandType.Text;
+                OracleDataAdapter da2 = new OracleDataAdapter(Cmd);
+                da2.Fill(dt2);
+                comboBoxRole.DisplayMember = dt2.Columns[0].ColumnName;
+                comboBoxRole.ValueMember = dt2.Columns[0].ColumnName;
+                comboBoxRole.DataSource = dt2;
+            }
+
+            catch
+            {
+                MessageBox.Show("Không tồn tại role nào để gán!");
+                this.Close();
+            }
         }
 
         private void buttonHuy_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void buttonGan_Click(object sender, EventArgs e)
+        {
+            string username = comboBoxUserName.SelectedValue.ToString();
+            string rolename = comboBoxRole.SelectedValue.ToString();
+            OracleConnection conn = new OracleConnection(connectionString);
+            conn.Open();
+            string text = "GRANT " + rolename + " TO " + username;
+            Console.WriteLine(text);
+            OracleCommand command = new OracleCommand(text, conn);
+            command.ExecuteNonQuery();
+
+            MessageBox.Show("Gán role thành công!", "Thông báo");
         }
     }
 }
--- ATBM_HTTT-N11/CODE/QL_DEAN/PHANHE1/GanRoleChoUser.designer.cs
+++ ATBM_HTTT-N11/CODE/QL_DEAN/PHANHE1/GanRoleChoUser.designer.cs
@@ -32,10 +32,10 @@
             this.labelGanRoleChoUser = new System.Windows.Forms.Label();
             this.buttonHuy = new System.Windows.Forms.Button();
             this.labelUsername = new System.Windows.Forms.Label();
-            this.textBoxUsername = new System.Windows.Forms.TextBox();
             this.labelRole = new System.Windows.Forms.Label();
             this.buttonGan = new System.Windows.Forms.Button();
             this.comboBoxRole = new System.Windows.Forms.ComboBox();
+            this.comboBoxUserName = new System.Windows.Forms.ComboBox();
             this.groupBox.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,18 +44,21 @@
             this.groupBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(237)))), ((int)(((byte)(199)))));
             this.groupBox.Controls.Add(this.labelGanRoleChoUser);
             this.groupBox.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.groupBox.Location = new System.Drawing.Point(0, -10);
+            this.groupBox.Location = new System.Drawing.Point(0, -8);
+            this.groupBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox.Name = "groupBox";
-            this.groupBox.Size = new System.Drawing.Size(1425, 160);
+            this.groupBox.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox.Size = new System.Drawing.Size(1069, 130);
             this.groupBox.TabIndex = 7;
             this.groupBox.TabStop = false;
             // 
             // labelGanRoleChoUser
             // 
             this.labelGanRoleChoUser.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.labelGanRoleChoUser.Location = new System.Drawing.Point(540, 50);
+            this.labelGanRoleChoUser.Location = new System.Drawing.Point(405, 41);
+            this.labelGanRoleChoUser.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelGanRoleChoUser.Name = "labelGanRoleChoUser";
-            this.labelGanRoleChoUser.Size = new System.Drawing.Size(350, 50);
+            this.labelGanRoleChoUser.Size = new System.Drawing.Size(262, 41);
             this.labelGanRoleChoUser.TabIndex = 0;
             this.labelGanRoleChoUser.Text = "Gán role cho user";
             // 
@@ -63,9 +66,10 @@
             // 
             this.buttonHuy.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(211)))), ((int)(((byte)(110)))));
             this.buttonHuy.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonHuy.Location = new System.Drawing.Point(850, 550);
+            this.buttonHuy.Location = new System.Drawing.Point(638, 447);
+            this.buttonHuy.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonHuy.Name = "buttonHuy";
-            this.buttonHuy.Size = new System.Drawing.Size(200, 70);
+            this.buttonHuy.Size = new System.Drawing.Size(150, 57);
             this.buttonHuy.TabIndex = 13;
             this.buttonHuy.Text = "Hủy";
             this.buttonHuy.UseVisualStyleBackColor = false;
@@ -75,29 +79,21 @@
             // 
             this.labelUsername.AutoSize = true;
             this.labelUsername.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.labelUsername.Location = new System.Drawing.Point(420, 210);
+            this.labelUsername.Location = new System.Drawing.Point(315, 171);
+            this.labelUsername.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelUsername.Name = "labelUsername";
-            this.labelUsername.Size = new System.Drawing.Size(158, 41);
+            this.labelUsername.Size = new System.Drawing.Size(128, 32);
             this.labelUsername.TabIndex = 14;
             this.labelUsername.Text = "Username";
-            // 
-            // textBoxUsername
-            // 
-            this.textBoxUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBoxUsername.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.textBoxUsername.Location = new System.Drawing.Point(420, 263);
-            this.textBoxUsername.Multiline = true;
-            this.textBoxUsername.Name = "textBoxUsername";
-            this.textBoxUsername.Size = new System.Drawing.Size(630, 49);
-            this.textBoxUsername.TabIndex = 15;
             // 
             // labelRole
             // 
             this.labelRole.AutoSize = true;
             this.labelRole.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.labelRole.Location = new System.Drawing.Point(420, 372);
+            this.labelRole.Location = new System.Drawing.Point(315, 302);
+            this.labelRole.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelRole.Name = "labelRole";
-            this.labelRole.Size = new System.Drawing.Size(80, 41);
+            this.labelRole.Size = new System.Drawing.Size(64, 32);
             this.labelRole.TabIndex = 16;
             this.labelRole.Text = "Role";
             // 
@@ -105,35 +101,49 @@
             // 
             this.buttonGan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(237)))), ((int)(((byte)(199)))));
             this.buttonGan.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.buttonGan.Location = new System.Drawing.Point(420, 550);
+            this.buttonGan.Location = new System.Drawing.Point(315, 447);
+            this.buttonGan.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonGan.Name = "buttonGan";
-            this.buttonGan.Size = new System.Drawing.Size(200, 70);
+            this.buttonGan.Size = new System.Drawing.Size(150, 57);
             this.buttonGan.TabIndex = 18;
             this.buttonGan.Text = "Gán";
             this.buttonGan.UseVisualStyleBackColor = false;
+            this.buttonGan.Click += new System.EventHandler(this.buttonGan_Click);
             // 
             // comboBoxRole
             // 
             this.comboBoxRole.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.comboBoxRole.FormattingEnabled = true;
-            this.comboBoxRole.Location = new System.Drawing.Point(420, 425);
+            this.comboBoxRole.Location = new System.Drawing.Point(315, 345);
+            this.comboBoxRole.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBoxRole.Name = "comboBoxRole";
-            this.comboBoxRole.Size = new System.Drawing.Size(630, 49);
+            this.comboBoxRole.Size = new System.Drawing.Size(474, 40);
             this.comboBoxRole.TabIndex = 19;
+            // 
+            // comboBoxUserName
+            // 
+            this.comboBoxUserName.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.comboBoxUserName.FormattingEnabled = true;
+            this.comboBoxUserName.Location = new System.Drawing.Point(315, 215);
+            this.comboBoxUserName.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxUserName.Name = "comboBoxUserName";
+            this.comboBoxUserName.Size = new System.Drawing.Size(474, 40);
+            this.comboBoxUserName.TabIndex = 20;
             // 
             // GanRoleChoUser
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.ClientSize = new System.Drawing.Size(1422, 977);
+            this.ClientSize = new System.Drawing.Size(1066, 794);
+            this.Controls.Add(this.comboBoxUserName);
             this.Controls.Add(this.comboBoxRole);
             this.Controls.Add(this.buttonGan);
             this.Controls.Add(this.labelRole);
-            this.Controls.Add(this.textBoxUsername);
             this.Controls.Add(this.labelUsername);
             this.Controls.Add(this.groupBox);
             this.Controls.Add(this.buttonHuy);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "GanRoleChoUser";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "GanRoleChoUser";
@@ -150,10 +160,10 @@
         private System.Windows.Forms.Button buttonHuy;
         private System.Windows.Forms.Label labelGanRoleChoUser;
         private System.Windows.Forms.Label labelUsername;
-        private System.Windows.Forms.TextBox textBoxUsername;
         private System.Windows.Forms.Label labelRole;
         private System.Windows.Forms.Button buttonGan;
         private System.Windows.Forms.ComboBox comboBoxRole;
+        private System.Windows.Forms.ComboBox comboBoxUserName;
     }
 }
 
