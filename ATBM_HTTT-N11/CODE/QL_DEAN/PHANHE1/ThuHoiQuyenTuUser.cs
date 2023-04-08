367ff3ce6845c41b7a76b2778750ace7315aae3f
Nguyen Tran Lan Trinh <lantrinh252002@gmail.com>
2023-04-08 23:24:09 +0700

    Thu hồi quyền từ user

--- ATBM_HTTT-N11/CODE/QL_DEAN/PHANHE1/ThuHoiQuyenTuUser.cs
+++ ATBM_HTTT-N11/CODE/QL_DEAN/PHANHE1/ThuHoiQuyenTuUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,19 +13,149 @@ namespace PHANHE1
 {
     public partial class ThuHoiQuyenTuUser : Form
     {
+        string connectionString = Login.connectionString;
+
+        string[] pri = new string[] { "SELECT", "INSERT", "UPDATE", "DELETE" };
         public ThuHoiQuyenTuUser()
         {
             InitializeComponent();
+            comboBoxThuHoiQuyen.Items.Add("SELECT");
+            comboBoxThuHoiQuyen.Items.Add("INSERT");
+            comboBoxThuHoiQuyen.Items.Add("UPDATE");
+            comboBoxThuHoiQuyen.Items.Add("DELETE");
+            comboBoxThuHoiQuyen.SelectedIndex = 0;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            OracleConnection conn = new OracleConnection(connectionString);
+            conn.Open();
+            try
+            {
 
+                DataTable dt2 = new DataTable();
+                string temp = "SELECT * FROM dba_users WHERE USERNAME NOT LIKE '%SYS%' AND ACCOUNT_STATUS = 'OPEN' ORDER BY created DESC";
+                OracleCommand Cmd = new OracleCommand(temp, conn);
+                Cmd.CommandType = CommandType.Text;
+                OracleDataAdapter da2 = new OracleDataAdapter(Cmd);
+                da2.Fill(dt2);
+                comboBoxUserName.DisplayMember = dt2.Columns[0].ColumnName;
+                comboBoxUserName.ValueMember = dt2.Columns[0].ColumnName;
+                comboBoxUserName.DataSource = dt2;
+            }
+
+            catch
+            {
+                MessageBox.Show("Không tồn tại user nào để thu hồi quyền!");
+            }
+
+            try
+            {
+                DataTable dt = new DataTable();
+                string temp = "select TABLE_NAME from USER_TABLES";
+                OracleCommand a_Cmd = new OracleCommand(temp, conn);
+                a_Cmd.CommandType = CommandType.Text;
+                OracleDataAdapter da = new OracleDataAdapter(a_Cmd);
+
+                da.Fill(dt);
+
+                comboBoxBang.DisplayMember = dt.Columns[0].ColumnName;
+                comboBoxBang.ValueMember = dt.Columns[0].ColumnName;
+                comboBoxBang.DataSource = dt;
+            }
+            catch
+            {
+                MessageBox.Show("Không tồn tại table nào để thu hồi quyền!");
+            }
+
+            conn.Close();
         }
 
         private void buttonHuy_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void labelCapQuyen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxUserName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBoxCapTrenCot_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxCapTrenCot.Checked)
+            {
+                comboBoxCot.DataSource = null;
+            }
+        }
+
+        private void comboBoxBang_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxThuHoiQuyen.SelectedIndex != 1 && comboBoxThuHoiQuyen.SelectedIndex != 3 && !checkBoxCapTrenCot.Checked)
+            {
+                OracleConnection conn = new OracleConnection(connectionString);
+                conn.Open();
+                string temp;
+                string table = comboBoxBang.SelectedValue.ToString();
+                temp = "SELECT column_name FROM USER_TAB_COLUMNS WHERE table_name = '" + table + "'";
+                DataTable dt2 = new DataTable();
+
+                OracleCommand Cmd = new OracleCommand(temp, conn);
+                Cmd.CommandType = CommandType.Text;
+                OracleDataAdapter da2 = new OracleDataAdapter(Cmd);
+
+                da2.Fill(dt2);
+                comboBoxCot.DisplayMember = dt2.Columns[0].ColumnName;
+                comboBoxCot.ValueMember = dt2.Columns[0].ColumnName;
+                comboBoxCot.DataSource = dt2;
+                conn.Close();
+            }
+        }
+
+        private void comboBoxThuHoiQuyen_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxThuHoiQuyen.SelectedIndex == 1 || comboBoxThuHoiQuyen.SelectedIndex == 3)
+            {
+                comboBoxCot.DataSource = null;
+            }
+        }
+
+        private void buttonThuHoi_Click(object sender, EventArgs e)
+        {
+            string table = comboBoxBang.SelectedValue.ToString();
+            int temp = comboBoxThuHoiQuyen.SelectedIndex;
+            string username = comboBoxUserName.SelectedValue.ToString();
+
+            if (comboBoxCot.SelectedValue != null && pri[temp] != "SELECT" && pri[temp] != "UPDATE")
+            {
+                MessageBox.Show("Chỉ được thu hồi quyền SELECT, UPDATE trên cột!");
+            }
+            else if (comboBoxCot.SelectedValue == null)
+            {
+                OracleConnection conn = new OracleConnection(connectionString);
+                conn.Open();
+                string text = "REVOKE " + pri[temp] + " ON " + table + " FROM " + username;
+                Console.WriteLine(text);
+                OracleCommand command = new OracleCommand(text, conn);
+                command.ExecuteNonQuery();
+
+                MessageBox.Show("Thu hồi quyền thành công!", "Thông báo");
+
+            }
+            else
+            {
+
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- ATBM_HTTT-N11/CODE/QL_DEAN/PHANHE1/ThuHoiQuyenTuUser.designer.cs
+++ ATBM_HTTT-N11/CODE/QL_DEAN/PHANHE1/ThuHoiQuyenTuUser.designer.cs
@@ -31,13 +31,16 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.labelThuHoiQuyenTuUser = new System.Windows.Forms.Label();
             this.buttonHuy = new System.Windows.Forms.Button();
-            this.labelUsername = new System.Windows.Forms.Label();
-            this.textBoxUsername = new System.Windows.Forms.TextBox();
-            this.labelThuHoiQuyen = new System.Windows.Forms.Label();
             this.buttonThuHoi = new System.Windows.Forms.Button();
-            this.labelBang = new System.Windows.Forms.Label();
+            this.checkBoxCapTrenCot = new System.Windows.Forms.CheckBox();
+            this.comboBoxUserName = new System.Windows.Forms.ComboBox();
+            this.comboBoxCot = new System.Windows.Forms.ComboBox();
+            this.comboBoxBang = new System.Windows.Forms.ComboBox();
             this.comboBoxThuHoiQuyen = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.labelCot = new System.Windows.Forms.Label();
+            this.labelBang = new System.Windows.Forms.Label();
+            this.labelCapQuyen = new System.Windows.Forms.Label();
+            this.labelUsername = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,18 +49,22 @@
             this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(237)))), ((int)(((byte)(199)))));
             this.groupBox1.Controls.Add(this.labelThuHoiQuyenTuUser);
             this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.groupBox1.Location = new System.Drawing.Point(0, -10);
+            this.groupBox1.Location = new System.Drawing.Point(0, -8);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1425, 160);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(1069, 130);
             this.groupBox1.TabIndex = 7;
             this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // labelThuHoiQuyenTuUser
             // 
             this.labelThuHoiQuyenTuUser.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.labelThuHoiQuyenTuUser.Location = new System.Drawing.Point(510, 50);
+            this.labelThuHoiQuyenTuUser.Location = new System.Drawing.Point(382, 41);
+            this.labelThuHoiQuyenTuUser.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelThuHoiQuyenTuUser.Name = "labelThuHoiQuyenTuUser";
-            this.labelThuHoiQuyenTuUser.Size = new System.Drawing.Size(450, 50);
+            this.labelThuHoiQuyenTuUser.Size = new System.Drawing.Size(338, 41);
             this.labelThuHoiQuyenTuUser.TabIndex = 0;
             this.labelThuHoiQuyenTuUser.Text = "Thu hồi quyền từ user";
             // 
@@ -65,98 +72,148 @@
             // 
             this.buttonHuy.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(211)))), ((int)(((byte)(110)))));
             this.buttonHuy.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonHuy.Location = new System.Drawing.Point(850, 700);
+            this.buttonHuy.Location = new System.Drawing.Point(638, 596);
+            this.buttonHuy.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonHuy.Name = "buttonHuy";
-            this.buttonHuy.Size = new System.Drawing.Size(200, 70);
+            this.buttonHuy.Size = new System.Drawing.Size(150, 57);
             this.buttonHuy.TabIndex = 13;
             this.buttonHuy.Text = "Hủy";
             this.buttonHuy.UseVisualStyleBackColor = false;
             this.buttonHuy.Click += new System.EventHandler(this.buttonHuy_Click);
             // 
-            // labelUsername
-            // 
-            this.labelUsername.AutoSize = true;
-            this.labelUsername.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.labelUsername.Location = new System.Drawing.Point(420, 200);
-            this.labelUsername.Name = "labelUsername";
-            this.labelUsername.Size = new System.Drawing.Size(158, 41);
-            this.labelUsername.TabIndex = 14;
-            this.labelUsername.Text = "Username";
-            // 
-            // textBoxUsername
-            // 
-            this.textBoxUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBoxUsername.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.textBoxUsername.Location = new System.Drawing.Point(420, 253);
-            this.textBoxUsername.Multiline = true;
-            this.textBoxUsername.Name = "textBoxUsername";
-            this.textBoxUsername.Size = new System.Drawing.Size(630, 49);
-            this.textBoxUsername.TabIndex = 15;
-            // 
-            // labelThuHoiQuyen
-            // 
-            this.labelThuHoiQuyen.AutoSize = true;
-            this.labelThuHoiQuyen.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.labelThuHoiQuyen.Location = new System.Drawing.Point(420, 362);
-            this.labelThuHoiQuyen.Name = "labelThuHoiQuyen";
-            this.labelThuHoiQuyen.Size = new System.Drawing.Size(220, 41);
-            this.labelThuHoiQuyen.TabIndex = 16;
-            this.labelThuHoiQuyen.Text = "Thu hồi quyền";
-            // 
             // buttonThuHoi
             // 
             this.buttonThuHoi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(237)))), ((int)(((byte)(199)))));
             this.buttonThuHoi.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.buttonThuHoi.Location = new System.Drawing.Point(420, 700);
+            this.buttonThuHoi.Location = new System.Drawing.Point(315, 596);
+            this.buttonThuHoi.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonThuHoi.Name = "buttonThuHoi";
-            this.buttonThuHoi.Size = new System.Drawing.Size(200, 70);
+            this.buttonThuHoi.Size = new System.Drawing.Size(150, 57);
             this.buttonThuHoi.TabIndex = 18;
             this.buttonThuHoi.Text = "Thu hồi";
             this.buttonThuHoi.UseVisualStyleBackColor = false;
+            this.buttonThuHoi.Click += new System.EventHandler(this.buttonThuHoi_Click);
+            // 
+            // checkBoxCapTrenCot
+            // 
+            this.checkBoxCapTrenCot.AutoSize = true;
+            this.checkBoxCapTrenCot.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.checkBoxCapTrenCot.Location = new System.Drawing.Point(51, 523);
+            this.checkBoxCapTrenCot.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBoxCapTrenCot.Name = "checkBoxCapTrenCot";
+            this.checkBoxCapTrenCot.Size = new System.Drawing.Size(253, 36);
+            this.checkBoxCapTrenCot.TabIndex = 38;
+            this.checkBoxCapTrenCot.Text = "Không chọn trên cột";
+            this.checkBoxCapTrenCot.UseVisualStyleBackColor = true;
+            this.checkBoxCapTrenCot.CheckedChanged += new System.EventHandler(this.checkBoxCapTrenCot_CheckedChanged);
+            // 
+            // comboBoxUserName
+            // 
+            this.comboBoxUserName.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.comboBoxUserName.FormattingEnabled = true;
+            this.comboBoxUserName.Location = new System.Drawing.Point(315, 177);
+            this.comboBoxUserName.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxUserName.Name = "comboBoxUserName";
+            this.comboBoxUserName.Size = new System.Drawing.Size(474, 40);
+            this.comboBoxUserName.TabIndex = 37;
+            this.comboBoxUserName.SelectedIndexChanged += new System.EventHandler(this.comboBoxUserName_SelectedIndexChanged);
+            // 
+            // comboBoxCot
+            // 
+            this.comboBoxCot.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.comboBoxCot.FormattingEnabled = true;
+            this.comboBoxCot.Location = new System.Drawing.Point(315, 519);
+            this.comboBoxCot.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxCot.Name = "comboBoxCot";
+            this.comboBoxCot.Size = new System.Drawing.Size(474, 40);
+            this.comboBoxCot.TabIndex = 36;
+            // 
+            // comboBoxBang
+            // 
+            this.comboBoxBang.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.comboBoxBang.FormattingEnabled = true;
+            this.comboBoxBang.Location = new System.Drawing.Point(315, 405);
+            this.comboBoxBang.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxBang.Name = "comboBoxBang";
+            this.comboBoxBang.Size = new System.Drawing.Size(474, 40);
+            this.comboBoxBang.TabIndex = 35;
+            this.comboBoxBang.SelectedIndexChanged += new System.EventHandler(this.comboBoxBang_SelectedIndexChanged);
+            // 
+            // comboBoxThuHoiQuyen
+            // 
+            this.comboBoxThuHoiQuyen.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.comboBoxThuHoiQuyen.FormattingEnabled = true;
+            this.comboBoxThuHoiQuyen.Location = new System.Drawing.Point(315, 291);
+            this.comboBoxThuHoiQuyen.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxThuHoiQuyen.Name = "comboBoxThuHoiQuyen";
+            this.comboBoxThuHoiQuyen.Size = new System.Drawing.Size(474, 40);
+            this.comboBoxThuHoiQuyen.TabIndex = 34;
+            this.comboBoxThuHoiQuyen.SelectedIndexChanged += new System.EventHandler(this.comboBoxThuHoiQuyen_SelectedIndexChanged);
+            // 
+            // labelCot
+            // 
+            this.labelCot.AutoSize = true;
+            this.labelCot.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.labelCot.Location = new System.Drawing.Point(315, 476);
+            this.labelCot.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelCot.Name = "labelCot";
+            this.labelCot.Size = new System.Drawing.Size(53, 32);
+            this.labelCot.TabIndex = 33;
+            this.labelCot.Text = "Cột";
             // 
             // labelBang
             // 
             this.labelBang.AutoSize = true;
             this.labelBang.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.labelBang.Location = new System.Drawing.Point(420, 524);
+            this.labelBang.Location = new System.Drawing.Point(315, 362);
+            this.labelBang.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelBang.Name = "labelBang";
-            this.labelBang.Size = new System.Drawing.Size(90, 41);
-            this.labelBang.TabIndex = 19;
+            this.labelBang.Size = new System.Drawing.Size(72, 32);
+            this.labelBang.TabIndex = 32;
             this.labelBang.Text = "Bảng";
             // 
-            // comboBoxThuHoiQuyen
+            // labelCapQuyen
             // 
-            this.comboBoxThuHoiQuyen.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBoxThuHoiQuyen.FormattingEnabled = true;
-            this.comboBoxThuHoiQuyen.Location = new System.Drawing.Point(420, 415);
-            this.comboBoxThuHoiQuyen.Name = "comboBoxThuHoiQuyen";
-            this.comboBoxThuHoiQuyen.Size = new System.Drawing.Size(630, 49);
-            this.comboBoxThuHoiQuyen.TabIndex = 20;
+            this.labelCapQuyen.AutoSize = true;
+            this.labelCapQuyen.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.labelCapQuyen.Location = new System.Drawing.Point(315, 248);
+            this.labelCapQuyen.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelCapQuyen.Name = "labelCapQuyen";
+            this.labelCapQuyen.Size = new System.Drawing.Size(178, 32);
+            this.labelCapQuyen.TabIndex = 31;
+            this.labelCapQuyen.Text = "Thu hồi quyền";
+            this.labelCapQuyen.Click += new System.EventHandler(this.labelCapQuyen_Click);
             // 
-            // comboBox2
+            // labelUsername
             // 
-            this.comboBox2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(420, 577);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(630, 49);
-            this.comboBox2.TabIndex = 21;
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.labelUsername.Location = new System.Drawing.Point(315, 134);
+            this.labelUsername.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(128, 32);
+            this.labelUsername.TabIndex = 30;
+            this.labelUsername.Text = "Username";
             // 
             // ThuHoiQuyenTuUser
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.ClientSize = new System.Drawing.Size(1422, 977);
-            this.Controls.Add(this.comboBox2);
+            this.ClientSize = new System.Drawing.Size(1066, 794);
+            this.Controls.Add(this.checkBoxCapTrenCot);
+            this.Controls.Add(this.comboBoxUserName);
+            this.Controls.Add(this.comboBoxCot);
+            this.Controls.Add(this.comboBoxBang);
             this.Controls.Add(this.comboBoxThuHoiQuyen);
+            this.Controls.Add(this.labelCot);
             this.Controls.Add(this.labelBang);
-            this.Controls.Add(this.buttonThuHoi);
-            this.Controls.Add(this.labelThuHoiQuyen);
-            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.labelCapQuyen);
             this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.buttonThuHoi);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.buttonHuy);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ThuHoiQuyenTuUser";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ThuHoiQuyenTuUser";
@@ -172,13 +229,16 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button buttonHuy;
         private System.Windows.Forms.Label labelThuHoiQuyenTuUser;
-        private System.Windows.Forms.Label labelUsername;
-        private System.Windows.Forms.TextBox textBoxUsername;
-        private System.Windows.Forms.Label labelThuHoiQuyen;
         private System.Windows.Forms.Button buttonThuHoi;
-        private System.Windows.Forms.Label labelBang;
+        private System.Windows.Forms.CheckBox checkBoxCapTrenCot;
+        private System.Windows.Forms.ComboBox comboBoxUserName;
+        private System.Windows.Forms.ComboBox comboBoxCot;
+        private System.Windows.Forms.ComboBox comboBoxBang;
         private System.Windows.Forms.ComboBox comboBoxThuHoiQuyen;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label labelCot;
+        private System.Windows.Forms.Label labelBang;
+        private System.Windows.Forms.Label labelCapQuyen;
+        private System.Windows.Forms.Label labelUsername;
     }
 }
 
