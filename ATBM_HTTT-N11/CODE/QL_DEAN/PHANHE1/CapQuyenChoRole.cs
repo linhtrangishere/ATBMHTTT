5aaae7a82e17090931de6fb3f81de7f197ac530e
Nguyen Tran Lan Trinh <lantrinh252002@gmail.com>
2023-04-08 17:23:57 +0700

    Cấp quyền cho role

--- ATBM_HTTT-N11/CODE/QL_DEAN/PHANHE1/CapQuyenChoRole.cs
+++ ATBM_HTTT-N11/CODE/QL_DEAN/PHANHE1/CapQuyenChoRole.cs
@@ -7,24 +7,145 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace PHANHE1
 {
     public partial class CapQuyenChoRole : Form
     {
+        string connectionString = Login.connectionString;
+
+        string[] pri = new string[] { "SELECT", "INSERT", "UPDATE", "DELETE"};
+
         public CapQuyenChoRole()
         {
             InitializeComponent();
+            comboBoxCapQuyen.Items.Add("SELECT");
+            comboBoxCapQuyen.Items.Add("INSERT");
+            comboBoxCapQuyen.Items.Add("UPDATE");
+            comboBoxCapQuyen.Items.Add("DELETE");
+            comboBoxCapQuyen.SelectedIndex = 0;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            OracleConnection conn = new OracleConnection(connectionString);
+            conn.Open();
+            try {
+         
+                DataTable dt2 = new DataTable();
+                string temp = "SELECT DISTINCT GRANTED_ROLE FROM DBA_ROLE_PRIVS WHERE GRANTED_ROLE LIKE 'RL_%'";
+                OracleCommand Cmd = new OracleCommand(temp, conn);
+                Cmd.CommandType = CommandType.Text;
+                OracleDataAdapter da2 = new OracleDataAdapter(Cmd);
+                da2.Fill(dt2);
+                comboTenRole.DisplayMember = dt2.Columns[0].ColumnName;
+                comboTenRole.ValueMember = dt2.Columns[0].ColumnName;
+                comboTenRole.DataSource = dt2;
+            }
 
+            catch {
+                MessageBox.Show("Không tồn tại role nào để cấp quyền!");
+            }
+
+            try {
+                DataTable dt = new DataTable();
+                string temp = "select TABLE_NAME from USER_TABLES";
+                OracleCommand a_Cmd = new OracleCommand(temp, conn);
+                a_Cmd.CommandType = CommandType.Text;
+                OracleDataAdapter da = new OracleDataAdapter(a_Cmd);
+
+                da.Fill(dt);
+
+                comboBoxBang.DisplayMember = dt.Columns[0].ColumnName;
+                comboBoxBang.ValueMember = dt.Columns[0].ColumnName;
+                comboBoxBang.DataSource = dt;
+            }
+            catch {
+                MessageBox.Show("Không tồn tại table nào để cấp quyền!");
+            }
+           
+            conn.Close();
         }
 
         private void buttonHuy_Click(object sender, EventArgs e)
         {
+
             this.Close();
         }
+        private void comboBoxCapQuyen_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxCapQuyen.SelectedIndex == 1|| comboBoxCapQuyen.SelectedIndex == 3)
+            {
+                comboBoxCot.DataSource = null;
+            }
+
+        }
+        private void comboBoxBang_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxCapQuyen.SelectedIndex != 1 && comboBoxCapQuyen.SelectedIndex != 3 && !checkBoxColumn.Checked)
+            {
+                OracleConnection conn = new OracleConnection(connectionString);
+                conn.Open();
+                string temp;
+                string table = comboBoxBang.SelectedValue.ToString();
+                temp = "SELECT column_name FROM USER_TAB_COLUMNS WHERE table_name = '" + table + "'";
+                DataTable dt2 = new DataTable();
+
+                OracleCommand Cmd = new OracleCommand(temp, conn);
+                Cmd.CommandType = CommandType.Text;
+                OracleDataAdapter da2 = new OracleDataAdapter(Cmd);
+
+                da2.Fill(dt2);
+
+                comboBoxCot.DisplayMember = dt2.Columns[0].ColumnName;
+                comboBoxCot.ValueMember = dt2.Columns[0].ColumnName;
+                comboBoxCot.DataSource = dt2;
+                conn.Close();
+            }
+
+        }
+
+        private void buttonThem_Click(object sender, EventArgs e)
+        {   
+           
+                string table = comboBoxBang.SelectedValue.ToString();
+                int temp = comboBoxCapQuyen.SelectedIndex;
+                string role = comboTenRole.SelectedValue.ToString();
+                
+                if (comboBoxCot.SelectedValue != null && pri[temp] != "SELECT" && pri[temp] != "UPDATE")
+                {
+                    MessageBox.Show("Chỉ được cấp quyền SELECT, DELETE trên cột!");
+                }
+                else if(comboBoxCot.SelectedValue == null)
+                {
+                    OracleConnection conn = new OracleConnection(connectionString);
+                    conn.Open();
+                string text = "GRANT " + pri[temp] + " ON " + table + " TO " + role;
+                Console.WriteLine(text);
+                OracleCommand command = new OracleCommand(text, conn);
+                command.ExecuteNonQuery();
+
+                MessageBox.Show("Phân quyền thành công!", "Thông báo");
+
+                }
+                else
+                {
+
+                }
+            
+          
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxColumn.Checked)
+            {
+                comboBoxCot.DataSource = null;
+            }
+        }
+
+        
     }
 }
--- ATBM_HTTT-N11/CODE/QL_DEAN/PHANHE1/CapQuyenChoRole.designer.cs
+++ ATBM_HTTT-N11/CODE/QL_DEAN/PHANHE1/CapQuyenChoRole.designer.cs
@@ -32,7 +32,6 @@
             this.labelCapQuyenChoRole = new System.Windows.Forms.Label();
             this.buttonHuy = new System.Windows.Forms.Button();
             this.labelTenRole = new System.Windows.Forms.Label();
-            this.textBoxTenRole = new System.Windows.Forms.TextBox();
             this.labelCapQuyen = new System.Windows.Forms.Label();
             this.buttonThem = new System.Windows.Forms.Button();
             this.labelBang = new System.Windows.Forms.Label();
@@ -40,6 +39,8 @@
             this.comboBoxCapQuyen = new System.Windows.Forms.ComboBox();
             this.comboBoxBang = new System.Windows.Forms.ComboBox();
             this.comboBoxCot = new System.Windows.Forms.ComboBox();
+            this.checkBoxColumn = new System.Windows.Forms.CheckBox();
+            this.comboTenRole = new System.Windows.Forms.ComboBox();
             this.groupBox.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -48,18 +49,21 @@
             this.groupBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(237)))), ((int)(((byte)(199)))));
             this.groupBox.Controls.Add(this.labelCapQuyenChoRole);
             this.groupBox.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.groupBox.Location = new System.Drawing.Point(0, -10);
+            this.groupBox.Location = new System.Drawing.Point(0, -8);
+            this.groupBox.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox.Name = "groupBox";
-            this.groupBox.Size = new System.Drawing.Size(1425, 160);
+            this.groupBox.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox.Size = new System.Drawing.Size(1069, 130);
             this.groupBox.TabIndex = 7;
             this.groupBox.TabStop = false;
             // 
             // labelCapQuyenChoRole
             // 
             this.labelCapQuyenChoRole.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.labelCapQuyenChoRole.Location = new System.Drawing.Point(530, 50);
+            this.labelCapQuyenChoRole.Location = new System.Drawing.Point(398, 41);
+            this.labelCapQuyenChoRole.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelCapQuyenChoRole.Name = "labelCapQuyenChoRole";
-            this.labelCapQuyenChoRole.Size = new System.Drawing.Size(400, 50);
+            this.labelCapQuyenChoRole.Size = new System.Drawing.Size(300, 41);
             this.labelCapQuyenChoRole.TabIndex = 0;
             this.labelCapQuyenChoRole.Text = "Cấp quyền cho role";
             // 
@@ -67,9 +71,10 @@
             // 
             this.buttonHuy.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(211)))), ((int)(((byte)(110)))));
             this.buttonHuy.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonHuy.Location = new System.Drawing.Point(848, 770);
+            this.buttonHuy.Location = new System.Drawing.Point(636, 626);
+            this.buttonHuy.Margin = new System.Windows.Forms.Padding(2);
             this.buttonHuy.Name = "buttonHuy";
-            this.buttonHuy.Size = new System.Drawing.Size(200, 70);
+            this.buttonHuy.Size = new System.Drawing.Size(150, 57);
             this.buttonHuy.TabIndex = 13;
             this.buttonHuy.Text = "Hủy";
             this.buttonHuy.UseVisualStyleBackColor = false;
@@ -79,29 +84,21 @@
             // 
             this.labelTenRole.AutoSize = true;
             this.labelTenRole.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.labelTenRole.Location = new System.Drawing.Point(418, 180);
+            this.labelTenRole.Location = new System.Drawing.Point(314, 146);
+            this.labelTenRole.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelTenRole.Name = "labelTenRole";
-            this.labelTenRole.Size = new System.Drawing.Size(137, 41);
+            this.labelTenRole.Size = new System.Drawing.Size(111, 32);
             this.labelTenRole.TabIndex = 14;
             this.labelTenRole.Text = "Tên Role";
-            // 
-            // textBoxTenRole
-            // 
-            this.textBoxTenRole.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBoxTenRole.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.textBoxTenRole.Location = new System.Drawing.Point(418, 233);
-            this.textBoxTenRole.Multiline = true;
-            this.textBoxTenRole.Name = "textBoxTenRole";
-            this.textBoxTenRole.Size = new System.Drawing.Size(630, 49);
-            this.textBoxTenRole.TabIndex = 15;
             // 
             // labelCapQuyen
             // 
             this.labelCapQuyen.AutoSize = true;
             this.labelCapQuyen.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.labelCapQuyen.Location = new System.Drawing.Point(418, 320);
+            this.labelCapQuyen.Location = new System.Drawing.Point(314, 260);
+            this.labelCapQuyen.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelCapQuyen.Name = "labelCapQuyen";
-            this.labelCapQuyen.Size = new System.Drawing.Size(167, 41);
+            this.labelCapQuyen.Size = new System.Drawing.Size(135, 32);
             this.labelCapQuyen.TabIndex = 16;
             this.labelCapQuyen.Text = "Cấp quyền";
             // 
@@ -109,20 +106,23 @@
             // 
             this.buttonThem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(237)))), ((int)(((byte)(199)))));
             this.buttonThem.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.buttonThem.Location = new System.Drawing.Point(418, 770);
+            this.buttonThem.Location = new System.Drawing.Point(314, 626);
+            this.buttonThem.Margin = new System.Windows.Forms.Padding(2);
             this.buttonThem.Name = "buttonThem";
-            this.buttonThem.Size = new System.Drawing.Size(200, 70);
+            this.buttonThem.Size = new System.Drawing.Size(150, 57);
             this.buttonThem.TabIndex = 18;
             this.buttonThem.Text = "Thêm";
             this.buttonThem.UseVisualStyleBackColor = false;
+            this.buttonThem.Click += new System.EventHandler(this.buttonThem_Click);
             // 
             // labelBang
             // 
             this.labelBang.AutoSize = true;
             this.labelBang.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.labelBang.Location = new System.Drawing.Point(418, 460);
+            this.labelBang.Location = new System.Drawing.Point(314, 374);
+            this.labelBang.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelBang.Name = "labelBang";
-            this.labelBang.Size = new System.Drawing.Size(90, 41);
+            this.labelBang.Size = new System.Drawing.Size(72, 32);
             this.labelBang.TabIndex = 19;
             this.labelBang.Text = "Bảng";
             // 
@@ -130,9 +130,10 @@
             // 
             this.labelCot.AutoSize = true;
             this.labelCot.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.labelCot.Location = new System.Drawing.Point(418, 600);
+            this.labelCot.Location = new System.Drawing.Point(314, 488);
+            this.labelCot.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelCot.Name = "labelCot";
-            this.labelCot.Size = new System.Drawing.Size(67, 41);
+            this.labelCot.Size = new System.Drawing.Size(53, 32);
             this.labelCot.TabIndex = 21;
             this.labelCot.Text = "Cột";
             // 
@@ -140,35 +141,65 @@
             // 
             this.comboBoxCapQuyen.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.comboBoxCapQuyen.FormattingEnabled = true;
-            this.comboBoxCapQuyen.Location = new System.Drawing.Point(418, 373);
+            this.comboBoxCapQuyen.Location = new System.Drawing.Point(314, 303);
+            this.comboBoxCapQuyen.Margin = new System.Windows.Forms.Padding(2);
             this.comboBoxCapQuyen.Name = "comboBoxCapQuyen";
-            this.comboBoxCapQuyen.Size = new System.Drawing.Size(630, 49);
+            this.comboBoxCapQuyen.Size = new System.Drawing.Size(474, 40);
             this.comboBoxCapQuyen.TabIndex = 22;
+            this.comboBoxCapQuyen.SelectedIndexChanged += new System.EventHandler(this.comboBoxCapQuyen_SelectedIndexChanged);
+           
             // 
             // comboBoxBang
             // 
             this.comboBoxBang.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.comboBoxBang.FormattingEnabled = true;
-            this.comboBoxBang.Location = new System.Drawing.Point(418, 513);
+            this.comboBoxBang.Location = new System.Drawing.Point(314, 417);
+            this.comboBoxBang.Margin = new System.Windows.Forms.Padding(2);
             this.comboBoxBang.Name = "comboBoxBang";
-            this.comboBoxBang.Size = new System.Drawing.Size(630, 49);
+            this.comboBoxBang.Size = new System.Drawing.Size(474, 40);
             this.comboBoxBang.TabIndex = 23;
+            this.comboBoxBang.SelectedIndexChanged += new System.EventHandler(this.comboBoxBang_SelectedIndexChanged);
             // 
             // comboBoxCot
             // 
             this.comboBoxCot.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.comboBoxCot.FormattingEnabled = true;
-            this.comboBoxCot.Location = new System.Drawing.Point(418, 653);
+            this.comboBoxCot.Location = new System.Drawing.Point(314, 531);
+            this.comboBoxCot.Margin = new System.Windows.Forms.Padding(2);
             this.comboBoxCot.Name = "comboBoxCot";
-            this.comboBoxCot.Size = new System.Drawing.Size(630, 49);
+            this.comboBoxCot.Size = new System.Drawing.Size(474, 40);
             this.comboBoxCot.TabIndex = 24;
+            // 
+            // checkBoxColumn
+            // 
+            this.checkBoxColumn.AutoSize = true;
+            this.checkBoxColumn.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxColumn.Location = new System.Drawing.Point(92, 537);
+            this.checkBoxColumn.Name = "checkBoxColumn";
+            this.checkBoxColumn.Size = new System.Drawing.Size(181, 34);
+            this.checkBoxColumn.TabIndex = 25;
+            this.checkBoxColumn.Text = "Không thêm cột";
+            this.checkBoxColumn.UseVisualStyleBackColor = true;
+            this.checkBoxColumn.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // comboTenRole
+            // 
+            this.comboTenRole.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.comboTenRole.FormattingEnabled = true;
+            this.comboTenRole.Location = new System.Drawing.Point(314, 189);
+            this.comboTenRole.Margin = new System.Windows.Forms.Padding(2);
+            this.comboTenRole.Name = "comboTenRole";
+            this.comboTenRole.Size = new System.Drawing.Size(474, 40);
+            this.comboTenRole.TabIndex = 26;
             // 
             // CapQuyenChoRole
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.ClientSize = new System.Drawing.Size(1422, 977);
+            this.ClientSize = new System.Drawing.Size(1066, 794);
+            this.Controls.Add(this.comboTenRole);
+            this.Controls.Add(this.checkBoxColumn);
             this.Controls.Add(this.comboBoxCot);
             this.Controls.Add(this.comboBoxBang);
             this.Controls.Add(this.comboBoxCapQuyen);
@@ -176,10 +207,10 @@
             this.Controls.Add(this.labelBang);
             this.Controls.Add(this.buttonThem);
             this.Controls.Add(this.labelCapQuyen);
-            this.Controls.Add(this.textBoxTenRole);
             this.Controls.Add(this.labelTenRole);
             this.Controls.Add(this.groupBox);
             this.Controls.Add(this.buttonHuy);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "CapQuyenChoRole";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CapQuyenChoRole";
@@ -196,7 +227,6 @@
         private System.Windows.Forms.Button buttonHuy;
         private System.Windows.Forms.Label labelCapQuyenChoRole;
         private System.Windows.Forms.Label labelTenRole;
-        private System.Windows.Forms.TextBox textBoxTenRole;
         private System.Windows.Forms.Label labelCapQuyen;
         private System.Windows.Forms.Button buttonThem;
         private System.Windows.Forms.Label labelBang;
@@ -204,6 +234,8 @@
         private System.Windows.Forms.ComboBox comboBoxCapQuyen;
         private System.Windows.Forms.ComboBox comboBoxBang;
         private System.Windows.Forms.ComboBox comboBoxCot;
+        private System.Windows.Forms.CheckBox checkBoxColumn;
+        private System.Windows.Forms.ComboBox comboTenRole;
     }
 }
 
